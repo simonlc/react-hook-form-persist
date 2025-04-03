54c78761a555b86658865c381e17811b7b4dbe74
Simon Laroche <hi@simon.lc>
2025-04-03 19:50:36 -0400

    Update readme, format test

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
+# @simonlc/react-hook-form-persist
 
-# react-hook-form-persistant
-[![package version](https://img.shields.io/npm/v/react-hook-form-persistant.svg?style=flat-square)](https://npmjs.org/package/react-hook-form-persistant)
-[![package downloads](https://img.shields.io/npm/dm/react-hook-form-persistant.svg?style=flat-square)](https://npmjs.org/package/react-hook-form-persistant)
+[![package version](https://img.shields.io/npm/v/@simonlc/react-hook-form-persist.svg?style=flat-square)](https://npmjs.org/package/@simonlc/react-hook-form-persist)
+[![package downloads](https://img.shields.io/npm/dm/@simonlc/react-hook-form-persist.svg?style=flat-square)](https://npmjs.org/package/@simonlc/react-hook-form-persist)
 [![standard-readme compliant](https://img.shields.io/badge/readme%20style-standard-brightgreen.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
-[![package license](https://img.shields.io/npm/l/react-hook-form-persistant.svg?style=flat-square)](https://npmjs.org/package/react-hook-form-persistant)
+[![package license](https://img.shields.io/npm/l/@simonlc/react-hook-form-persist.svg?style=flat-square)](https://npmjs.org/package/@simonlc/react-hook-form-persist)
 [![make a pull request](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
 
 Persist and populate [react-hook-form](https://react-hook-form.com/) form using storage of your choice
 
 ## 📖 Table of Contents
 
-- [react-hook-form-persistant](#react-hook-form-persistant)
+- [react-hook-form-persist](#react-hook-form-persist)
   - [📖 Table of Contents](#-table-of-contents)
   - [⚙️ Install](#️-install)
   - [📖 Usage](#-usage)
@@ -24,18 +24,21 @@ Persist and populate [react-hook-form](https://react-hook-form.com/) form using 
 Install the package locally within you project folder with your package manager:
 
 With `npm`:
+
 ```sh
-npm install react-hook-form-persistant
+npm install @simonlc/react-hook-form-persist
 ```
 
 With `yarn`:
+
 ```sh
-yarn add react-hook-form-persistant
+yarn add @simonlc/react-hook-form-persist
 ```
 
 With `pnpm`:
+
 ```sh
-pnpm add react-hook-form-persistant
+pnpm add @simonlc/react-hook-form-persist
 ```
 
 ## 📖 Usage
@@ -45,34 +48,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
 
-import useFormPersist from 'react-hook-form-persistant'
+import useFormPersist from "@simonlc/react-hook-form-persist";
 
 function App() {
   const { register, handleSubmit, watch, errors, setValue } = useForm();
 
   useFormPersist("storageKey", {
-    watch, 
+    watch,
     setValue,
     storage: window.localStorage, // default window.sessionStorage
-    exclude: ['baz']
+    exclude: ["baz"],
   });
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label>foo:
+      <label>
+        foo:
         <input name="foo" ref={register} />
       </label>
 
-      <label>bar (required):
+      <label>
+        bar (required):
         <input name="bar" ref={register({ required: true })} />
       </label>
       {errors.required && <span>This field is required</span>}
 
-      <label>baz (excluded):
+      <label>
+        baz (excluded):
         <input name="baz" ref={register} />
       </label>
 
@@ -83,7 +89,6 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
 ```
 
 ### Additional examples
@@ -91,34 +96,29 @@ ReactDOM.render(<App />, rootElement);
 Persist all form fields:
 
 ```js
-useFormPersist('form', {watch, setValue});
+useFormPersist("form", { watch, setValue });
 ```
 
 Persist all form fields except password:
 
 ```js
-useFormPersist('form', {watch, setValue, exclude: ['password']});
+useFormPersist("form", { watch, setValue, exclude: ["password"] });
 ```
 
 Persist only the email field:
 
 ```js
-useFormPersist('form', {watch, setValue, include: ['email'] });
+useFormPersist("form", { watch, setValue, include: ["email"] });
 ```
-
-
-
-
 
 ## 📚 API
 
-For all configuration options, please see the [API docs](https://paka.dev/npm/react-hook-form-persistant).
+TODO
 
 ## 💬 Contributing
 
-Got an idea for a new feature? Found a bug? Contributions are welcome! Please [open up an issue](https://github.com/kyzegs/feature-flip/issues) or [make a pull request](https://makeapullrequest.com/).
+Got an idea for a new feature? Found a bug? Contributions are welcome! Please [open up an issue](https://github.com/simonlc/react-hook-form-persist/issues) or [make a pull request](https://makeapullrequest.com/).
 
 ## 🪪 License
 
 [MIT © Tiaan du Plessis](./LICENSE)
-    
--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,178 +1,205 @@
-import React from 'react';
-import { describe, expect, test, vi } from 'vitest';
-import { render, renderHook, screen } from '@testing-library/react';
-import { useForm } from 'react-hook-form';
-import { type UseFormProps } from 'react-hook-form/dist/types';
-import userEvent from '@testing-library/user-event';
-import useFormPersist, { type FormPersistConfig } from '../src';
+import React from "react";
+import { describe, expect, test, vi } from "vitest";
+import { render, renderHook, screen } from "@testing-library/react";
+import { useForm } from "react-hook-form";
+import { type UseFormProps } from "react-hook-form/dist/types";
+import userEvent from "@testing-library/user-event";
+import useFormPersist, { type FormPersistConfig } from "../src";
 
-const STORAGE_KEY = 'STORAGE_KEY';
+const STORAGE_KEY = "STORAGE_KEY";
 
-const Form = ({ config = {}, props = {} }: { config?: Partial<FormPersistConfig>; props?: UseFormProps }) => {
-    const { register, watch, setValue } = useForm(props);
+const Form = ({
+  config = {},
+  props = {},
+}: {
+  config?: Partial<FormPersistConfig>;
+  props?: UseFormProps;
+}) => {
+  const { register, watch, setValue } = useForm(props);
 
-    useFormPersist(STORAGE_KEY, { watch, setValue, ...config });
+  useFormPersist(STORAGE_KEY, { watch, setValue, ...config });
 
-    return (
-        <form>
-            <label>
-                 foo:
-                <input id='foo' {...register('foo')} />
-            </label>
-            <label>
-                bar:
-                <input id='bar' {...register('bar')} />
-            </label>
-            <label>
-                 baz:
-                <input id='baz' {...register('baz')} />
-            </label>
-            <label>
-                qux:
-                <input id='qux' {...register('qux.quux')} />
-            </label>
-            <button type='submit'>submit</button>
-        </form>
-    );
+  return (
+    <form>
+      <label>
+        foo:
+        <input id="foo" {...register("foo")} />
+      </label>
+      <label>
+        bar:
+        <input id="bar" {...register("bar")} />
+      </label>
+      <label>
+        baz:
+        <input id="baz" {...register("baz")} />
+      </label>
+      <label>
+        qux:
+        <input id="qux" {...register("qux.quux")} />
+      </label>
+      <button type="submit">submit</button>
+    </form>
+  );
 };
 
-describe('react-hook-form-persistant', () => {
-    test('should persist fields in storage', async () => {
-        const spy = vi.spyOn(Storage.prototype, 'setItem');
+describe("react-hook-form-persist", () => {
+  test("should persist fields in storage", async () => {
+    const spy = vi.spyOn(Storage.prototype, "setItem");
 
-        render(<Form />);
+    render(<Form />);
 
-        await userEvent.type(screen.getByLabelText('foo:'), 'foo');
+    await userEvent.type(screen.getByLabelText("foo:"), "foo");
 
-        expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
 
-        expect(JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) ?? '{}')).toEqual({
-            foo: 'foo',
-            bar: '',
-            baz: '',
-            qux: {
-                quux: '',
-            },
-        });
+    expect(
+      JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) ?? "{}")
+    ).toEqual({
+      foo: "foo",
+      bar: "",
+      baz: "",
+      qux: {
+        quux: "",
+      },
+    });
+  });
+
+  test("should retrieve stored fields", async () => {
+    const spy = vi.spyOn(Storage.prototype, "getItem");
+
+    const { unmount } = render(<Form />);
+
+    await userEvent.type(screen.getByLabelText("foo:"), "foo");
+
+    unmount();
+    render(<Form />);
+
+    expect(spy).toHaveBeenCalled();
+    expect(screen.getByLabelText("foo:")).toHaveValue("foo");
+  });
+
+  test("should not persist excluded fields", async () => {
+    render(<Form config={{ exclude: ["baz", "foo", "qux.quux"] }} />);
+
+    await userEvent.type(screen.getByLabelText("foo:"), "foo");
+    await userEvent.type(screen.getByLabelText("bar:"), "bar");
+    await userEvent.type(screen.getByLabelText("baz:"), "baz");
+    await userEvent.type(screen.getByLabelText("qux:"), "qux");
+
+    expect(
+      JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) ?? "{}")
+    ).toEqual({
+      bar: "bar",
+    });
+  });
+
+  test("should support timeout config option", async () => {
+    const now = Date.now();
+    const { unmount } = render(<Form config={{ timeout: 1000 }} />);
+
+    const spy = vi.spyOn(Date, "now").mockReturnValue(now);
+
+    await userEvent.type(screen.getByLabelText("foo:"), "foo");
+    await userEvent.type(screen.getByLabelText("bar:"), "bar");
+    await userEvent.type(screen.getByLabelText("baz:"), "baz");
+    await userEvent.type(screen.getByLabelText("qux:"), "qux");
+
+    expect(spy).toBeCalled();
+    expect(
+      JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) ?? "{}")
+    ).toEqual({
+      bar: "bar",
+      baz: "baz",
+      foo: "foo",
+      qux: {
+        quux: "qux",
+      },
+      _timestamp: now,
     });
 
-    test('should retrieve stored fields', async () => {
-        const spy = vi.spyOn(Storage.prototype, 'getItem');
+    unmount();
+    spy.mockImplementation(() => now + 4000);
+    const clearSpy = vi.spyOn(Storage.prototype, "removeItem");
 
-        const { unmount } = render(<Form />);
+    render(<Form config={{ timeout: 1000 }} />);
 
-        await userEvent.type(screen.getByLabelText('foo:'), 'foo');
+    expect(clearSpy).toBeCalled();
+    expect(
+      JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) ?? "{}")
+    ).toEqual({});
+  });
 
-        unmount();
-        render(<Form />);
+  test("should not set value if diff is equal", async () => {
+    const setValue = vi.fn(() => {});
 
-        expect(spy).toHaveBeenCalled();
-        expect(screen.getByLabelText('foo:')).toHaveValue('foo');
+    window.sessionStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify({
+        foo: "bar",
+        qux: { quux: "qux" },
+      })
+    );
+
+    render(
+      <Form
+        config={{ setValue }}
+        props={{
+          defaultValues: {
+            foo: "bar",
+            qux: { quux: "qux" },
+          },
+        }}
+      />
+    );
+
+    expect(setValue).toHaveBeenCalledTimes(0);
+  });
+
+  test("should set value if diff is different", async () => {
+    const setValue = vi.fn(() => {});
+
+    window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify({ foo: "baz" }));
+
+    render(
+      <Form config={{ setValue }} props={{ defaultValues: { foo: "bar" } }} />
+    );
+
+    expect(setValue).toHaveBeenCalledOnce();
+  });
+
+  test("should call onDataRestored callback", async () => {
+    const onDataRestored = vi.fn(() => {});
+
+    window.sessionStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify({
+        foo: "bar",
+        qux: { quux: "qux" },
+      })
+    );
+
+    render(<Form config={{ onDataRestored }} />);
+
+    expect(onDataRestored).toHaveBeenCalledOnce();
+    expect(onDataRestored).toHaveBeenCalledWith({
+      foo: "bar",
+      qux: { quux: "qux" },
     });
+  });
 
-    test('should not persist excluded fields', async () => {
-        render(<Form config={{ exclude: ['baz', 'foo', 'qux.quux'] }} />);
+  test("should clear storage", async () => {
+    window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify({ foo: "baz" }));
 
-        await userEvent.type(screen.getByLabelText('foo:'), 'foo');
-        await userEvent.type(screen.getByLabelText('bar:'), 'bar');
-        await userEvent.type(screen.getByLabelText('baz:'), 'baz');
-        await userEvent.type(screen.getByLabelText('qux:'), 'qux');
+    const { result: formResult } = renderHook(() => useForm());
+    const { result: formPersistResult } = renderHook(() =>
+      useFormPersist(STORAGE_KEY, {
+        watch: formResult.current.watch,
+        setValue: formResult.current.setValue,
+      })
+    );
 
-        expect(JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) ?? '{}')).toEqual({
-            bar: 'bar',
-        });
-    });
+    formPersistResult.current.clear();
 
-    test('should support timeout config option', async () => {
-        const now = Date.now();
-        const { unmount } = render(<Form config={{ timeout: 1000 }} />);
-
-        const spy = vi.spyOn(Date, 'now').mockReturnValue(now);
-
-        await userEvent.type(screen.getByLabelText('foo:'), 'foo');
-        await userEvent.type(screen.getByLabelText('bar:'), 'bar');
-        await userEvent.type(screen.getByLabelText('baz:'), 'baz');
-        await userEvent.type(screen.getByLabelText('qux:'), 'qux');
-
-        expect(spy).toBeCalled();
-        expect(JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) ?? '{}')).toEqual({
-            bar: 'bar',
-            baz: 'baz',
-            foo: 'foo',
-            qux: {
-                quux: 'qux',
-            },
-            _timestamp: now,
-        });
-
-        unmount();
-        spy.mockImplementation(() => now + 4000);
-        const clearSpy = vi.spyOn(Storage.prototype, 'removeItem');
-
-        render(<Form config={{ timeout: 1000 }} />);
-
-        expect(clearSpy).toBeCalled();
-        expect(JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) ?? '{}')).toEqual({});
-    });
-
-    test('should not set value if diff is equal', async () => {
-        const setValue = vi.fn(() => {});
-
-        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify({
-            foo: 'bar',
-            qux: { quux: 'qux' },
-        }));
-
-        render(
-            <Form
-                config={{ setValue }}
-                props={{
-                    defaultValues: {
-                        foo: 'bar',
-                        qux: { quux: 'qux' },
-                    },
-                }}
-            />,
-        );
-
-        expect(setValue).toHaveBeenCalledTimes(0);
-    });
-
-    test('should set value if diff is different', async () => {
-        const setValue = vi.fn(() => {});
-
-        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify({ foo: 'baz' }));
-
-        render(<Form config={{ setValue }} props={{ defaultValues: { foo: 'bar' } }} />);
-
-        expect(setValue).toHaveBeenCalledOnce();
-    });
-
-    test('should call onDataRestored callback', async () => {
-        const onDataRestored = vi.fn(() => {});
-
-        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify({
-            foo: 'bar',
-            qux: { quux: 'qux' },
-        }));
-
-        render(<Form config={{ onDataRestored }} />);
-
-        expect(onDataRestored).toHaveBeenCalledOnce();
-        expect(onDataRestored).toHaveBeenCalledWith({ foo: 'bar', qux: { quux: 'qux' } });
-    });
-
-    test('should clear storage', async () => {
-        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify({ foo: 'baz' }));
-
-        const { result: formResult } = renderHook(() => useForm());
-        const { result: formPersistResult } = renderHook(() => useFormPersist(STORAGE_KEY, {
-            watch: formResult.current.watch,
-            setValue: formResult.current.setValue,
-        }));
-
-        formPersistResult.current.clear();
-
-        expect(window.sessionStorage.getItem(STORAGE_KEY)).toBeNull();
-    });
+    expect(window.sessionStorage.getItem(STORAGE_KEY)).toBeNull();
+  });
 });
